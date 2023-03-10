b8888d56f42e93952d017c33830b41cd2301408d
ciraos <frank.ka@foxmail.com>
2023-03-10 23:51:02 +0800

    u 页脚新增随机友链

---
+++ js/index.js
@@ -0,0 +1,15 @@
+/* 随机友链 */
+fetch('http://fcircle.itciraos.cn/friend')
+    .then(response => response.json())
+    .then(data => {
+        const friendList = document.getElementById('friend-list');
+        for (let i = 0; i < 3; i++) {
+            const randomIndex = Math.floor(Math.random() * data.length);
+            const friend = data[randomIndex];
+            const li = document.createElement('li');
+            li.innerHTML = `<a target="_blank" rel="noopener noreferrer" href="${friend.link}">${friend.name}</a>`;
+            friendList.appendChild(li);
+        }
+    });
+
+/** */
