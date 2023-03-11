23d2f56ef4f957209dac47011be1803d348fb473
ciraos <frank.ka@foxmail.com>
2023-03-11 14:16:21 +0800

    d 移除页脚随机友链

--- js/index.js
+++ js/index.js
@@ -1,13 +1,1 @@
-/* 随机友链 */
-fetch('https://fcircle.itciraos.cn/randomfriend?num=3')
-    .then(response => response.json())
-    .then(data => {
-        const friendList = document.getElementById('friend-list');
-        data.friends.forEach(friend => {
-            const li = document.createElement('li');
-            li.innerHTML = `<a target="_blank" rel="noopener noreferrer" href="${friend.link}">${friend.name}</a>`;
-            friendList.appendChild(li);
-        });
-    });
-
 /** */
