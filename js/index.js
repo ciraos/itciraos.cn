8120de6aa5f82a8b9b9ad66dc1f0c903ae15a15f
ciraos <frank.ka@foxmail.com>
2023-03-11 14:14:08 +0800

    f 修正朋友圈api

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ fetch('https://fcircle.itciraos.cn/randomfriend?num=3')
         const friendList = document.getElementById('friend-list');
         data.friends.forEach(friend => {
             const li = document.createElement('li');
-            li.innerHTML = `<a target="_blank" rel="noopener noreferrer" href="${friend.name}">${friend.link}</a>`;
+            li.innerHTML = `<a target="_blank" rel="noopener noreferrer" href="${friend.link}">${friend.name}</a>`;
             friendList.appendChild(li);
         });
     });
